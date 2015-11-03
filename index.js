851f05824aab4df4946ec6fc1cd53b38bc751532
Matt Brennan <mattyb1000@gmail.com>
2015-11-03 21:54:40 +0000

    dedupe transactions

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var promisify = require('@quarterto/promisify');
 
 var writeFile = promisify(fs.writeFile);
 
+function toObject(tx) {
+	var out = {};
+	tx.forEach(function(t) {
+		out[JSON.stringify(t)] = t;
+	});
+	return out;
+}
+
+function values(obj) {
+	return Object.keys(obj).map(k => obj[k])
+}
+
 module.exports = function(filename, options) {
 	console.assert(path.extname(filename) === '.json', 'expected .json, got ' + path.extname(filename));
 
@@ -17,17 +29,14 @@ module.exports = function(filename, options) {
 		return new Date(Math.max(l, new Date(tx.date)));
 	}, 0);
 
-	console.log(latest);
+	var txObj = toObject(transactions);
 
 	if(latest > 0) {
 		options.earliest = latest; // eh whatever
 	}
 
 	return stepford(options).then(function(newTx) {
-		var allTx = transactions.concat(newTx).sort(function(t1, t2) {
-			return t1.date - t2.date;
-		});
-
+		var allTx = values(Object.assign(txObj, toObject(newTx))).sort((t1, t2) => t1.date - t2.date);
 		return writeFile(filename, JSON.stringify(allTx), 'utf-8');
 	});
 };
