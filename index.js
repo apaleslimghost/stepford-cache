00277a1fc7bc843b17c1d8709d2a062e608326e3
Matt Brennan <mattyb1000@gmail.com>
2015-11-28 11:38:20 +0000

    object hash when merging

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var stepford = require('stepford');
 var fs = require('fs');
 var path = require('path');
 var promisify = require('@quarterto/promisify');
+var hash = require('object-hash');
 
 var writeFile = promisify(fs.writeFile);
 
 function toObject(tx) {
 	var out = {};
 	tx.forEach(function(t) {
-		out[JSON.stringify(t)] = t;
+		out[hash(t)] = t;
 	});
 	return out;
 }
